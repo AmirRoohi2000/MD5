d0f16b14903a3327c3ff55b7d47ef0bc4a8bc380
AmirRoohi2000 <ahroohi1379@hotmail.com>
2018-12-14 17:21:06 +0100

    Add files via upload
    the wordlists are in the Debug directory, you will need all four, otherwise, this will not work, or just remove the code for them, have fun

---
+++ SimpleMD5Cracker/SimpleMD5Cracker/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace SimpleMD5Cracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMD5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPasswd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCrack = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMD5
+            // 
+            this.txtMD5.Location = new System.Drawing.Point(110, 12);
+            this.txtMD5.Name = "txtMD5";
+            this.txtMD5.Size = new System.Drawing.Size(435, 32);
+            this.txtMD5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MD5 Hash";
+            // 
+            // txtPasswd
+            // 
+            this.txtPasswd.Location = new System.Drawing.Point(172, 50);
+            this.txtPasswd.Name = "txtPasswd";
+            this.txtPasswd.Size = new System.Drawing.Size(373, 32);
+            this.txtPasswd.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cracked Password";
+            // 
+            // btnCrack
+            // 
+            this.btnCrack.AutoSize = true;
+            this.btnCrack.Location = new System.Drawing.Point(460, 88);
+            this.btnCrack.Name = "btnCrack";
+            this.btnCrack.Size = new System.Drawing.Size(85, 33);
+            this.btnCrack.TabIndex = 4;
+            this.btnCrack.Text = "Crack!";
+            this.btnCrack.UseVisualStyleBackColor = true;
+            this.btnCrack.Click += new System.EventHandler(this.btnCrack_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.AutoSize = true;
+            this.btnBrowse.Location = new System.Drawing.Point(7, 88);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(196, 33);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse for wordlists";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(557, 128);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnCrack);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPasswd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMD5);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimpleMD5Cracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMD5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPasswd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCrack;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ SimpleMD5Cracker/SimpleMD5Cracker/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace SimpleMD5Cracker
+{
+    public partial class Form1 : Form
+    {
+        string wordlsistPath;
+        string rock;
+        string en;
+        string de;
+        string top;
+        string Hash;
+        string Pass = "";
+        bool close_loop = false;
+        bool start = false;
+
+        StreamReader file;
+        StreamReader file1;
+        StreamReader file2;
+        StreamReader file3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrack_Click(object sender, EventArgs e)
+        {
+            if (txtMD5.Text == "")
+            {
+                start = false;
+            }
+            else
+            {
+                start = true;
+            }
+
+            Hash = txtMD5.Text.ToUpper();
+            txtPasswd.Clear();
+
+            file = new StreamReader(Rock);
+            file1 = new StreamReader(TOP);
+            file2 = new StreamReader(EN);
+            file3 = new StreamReader(DE);
+
+            if(start == true)
+            {
+                try
+                {
+                    while (close_loop == false && (Pass = file.ReadLine()) != null)
+                    {
+                        if (MD5Hash(Pass).Equals(Hash))
+                        {
+                            txtPasswd.Text = Pass;
+                            closeAll();
+                            close_loop = true;
+                        }
+                        else
+                        {
+                            Pass = file1.ReadLine();
+                            if (MD5Hash(Pass).Equals(Hash))
+                            {
+                                txtPasswd.Text = Pass;
+                                closeAll();
+                                close_loop = true;
+                            }
+                            else
+                            {
+                                Pass = file2.ReadLine();
+                                if (MD5Hash(Pass).Equals(Hash))
+                                {
+                                    txtPasswd.Text = Pass;
+                                    closeAll();
+                                    close_loop = true;
+                                }
+                                else
+                                {
+                                    Pass = file3.ReadLine();
+                                    if (MD5Hash(Pass).Equals(Hash))
+                                    {
+                                        txtPasswd.Text = Pass;
+                                        closeAll();
+                                        close_loop = true;
+                                    }
+                                    else
+                                    {
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must type in the hash!");
+            }
+
+            close_loop = false;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Choose the path to the wordlists";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                wordlsistPath = Path.GetFullPath(dialog.SelectedPath);
+                rock = wordlsistPath + @"\rockyou.txt";
+                en = wordlsistPath + @"\english.txt";
+                de = wordlsistPath + @"\german.txt";
+                top = wordlsistPath + @"\top.txt";
+                if (!File.Exists(rock))
+                {
+                    MessageBox.Show("Rockyou wasn/'t found!");
+                }
+                if (!File.Exists(en))
+                {
+                    MessageBox.Show("english wasn/'t found!");
+                }
+                if (!File.Exists(de))
+                {
+                    MessageBox.Show("german wasn/'t found!");
+                }
+                if (!File.Exists(top))
+                {
+                    MessageBox.Show("Top wasn/'t found!");
+                }
+            }
+        }
+
+        public string Rock
+        {
+            get
+            {
+                return rock;
+            }
+        }
+
+        public string EN
+        {
+            get
+            {
+                return en;
+            }
+        }
+
+        public string DE
+        {
+            get
+            {
+                return de;
+            }
+        }
+
+        public string TOP
+        {
+            get
+            {
+                return top;
+            }
+        }
+
+        private static string MD5Hash(string md5)
+        {
+            StringBuilder builder = new StringBuilder();
+            MD5CryptoServiceProvider mD5Crypto = new MD5CryptoServiceProvider();
+            byte[] bytes = mD5Crypto.ComputeHash(new UTF8Encoding().GetBytes(md5));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("X2"));
+            }
+            return builder.ToString();
+        }
+
+        private void closeAll()
+        {
+            file.Close();
+            file1.Close();
+            file2.Close();
+            file3.Close();
+        }
+    }
+}
